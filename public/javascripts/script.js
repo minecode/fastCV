9886d49e31ad933d16a0c61135b2ad7532182fed
hec1av <fixed-term.Claudio.Henriques@pt.bosch.com>
2020-02-03 17:12:57 +0000

    Push changes on script

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -40,28 +40,29 @@ const addModule = (module, contentModule) => {
 	var modules = document.getElementById('modules');
 	var div = document.createElement('div');
 	div.setAttribute('id', module + 'Div');
-	div.setAttribute('class', 'col-auto');
+	div.setAttribute('class', 'col-4');
 	var content = JSON.parse(contentModule);
 	var h4 = document.createElement('h4');
 	var title = document.createTextNode(module.replace('_', ' ').split(' ').map((word) => word.charAt(0).toUpperCase() + word.slice(1)).join(' '));
 	h4.append(title);
 	div.append(h4);
 	var form = document.createElement('form');
-	for (var element in content) {
-		var formDiv = document.createElement('div');
-		formDiv.setAttribute('class', 'form-group col-12');
-		var label = document.createElement('label');
-		label.setAttribute('for', element + 'Form');
-		var text = document.createTextNode(element);
-		label.append(text);
-		formDiv.append(label);
-		var input = document.createElement('input');
-		input.setAttribute('type', 'text');
-		input.setAttribute('class', 'form-control');
-		input.setAttribute('id', element + 'Form');
-		formDiv.append(input);
-		form.append(formDiv);
+	var formDiv = document.createElement('div');
+	formDiv.setAttribute('class', 'form-group col-12');
+	var label = document.createElement('label');
+	label.setAttribute('for', 'for' + module);
+	var select = document.createElement('select');
+	select.setAttribute('id', 'for' + module);
+	select.setAttribute('onChange', 'appendFormDiv(form, content, this.selectedIndex + 1)');
+	for (var i = 0; i < content.max; i++) {
+		var option = document.createElement('option');
+		var text = document.createTextNode(i + 1);
+		option.append(text);
+		select.append(option);
 	}
+	label.append(select);
+	formDiv.append(label);
+	form.append(formDiv);
 	div.append(form);
 	modules.append(div);
 };
@@ -73,4 +74,33 @@ const removeModule = (module, contentModule) => {
 	button.setAttribute('onclick', 'addModule(\'' + module + '\', ' + JSON.stringify(contentModule) + ')');
 	var div = document.getElementById(module + 'Div');
 	div.parentNode.removeChild(div);
+};
+
+const appendFormDiv = (form, content, count) => {
+	console.log('======================');
+	console.log(form);
+	console.log('======================');
+	console.log(content);
+	console.log('======================');
+	console.log(count);
+	console.log('======================');
+	for (var i = 0; i < count; i++) {
+		for (var element in content) {
+			if (element != 'max') {
+				var formDiv = document.createElement('div');
+				formDiv.setAttribute('class', 'form-group col-12');
+				var label = document.createElement('label');
+				label.setAttribute('for', element + 'Form');
+				var text = document.createTextNode(element);
+				label.append(text);
+				formDiv.append(label);
+				var input = document.createElement('input');
+				input.setAttribute('type', 'text');
+				input.setAttribute('class', 'form-control');
+				input.setAttribute('id', element + 'Form');
+				formDiv.append(input);
+				form.append(formDiv);
+			}
+		}
+	}
 };
